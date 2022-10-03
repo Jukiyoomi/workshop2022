e10d58bee92f04e7293f12028946c99a5a717962
Erwan8989 <77726753+Erwan8989@users.noreply.github.com>
2022-10-03 16:03:31 +0200

    Delete client

--- php/index.php
+++ php/index.php
@@ -2,22 +2,17 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8" />
-    <link rel="icon" type="image/svg+xml" href="../client/Image/favicon.png" />
+    <link rel="icon" type="image/svg+xml" href="../images/favicon.png" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Workshop 2022</title>
 </head>
 <body>
-<div class="container">
+<div class="container-fluid">
     <div class="row">
-        <div class='col-3'>
-            <?php
-            echo "Hello World";
-            ?>
-        </div>
-
-        <div class='col-9'>
-        </div>
+        <?php
+        include "../html/navbar.html"
+        ?>
     </div>
 </div>
 <script type="module" src="./js/main.js"></script>
