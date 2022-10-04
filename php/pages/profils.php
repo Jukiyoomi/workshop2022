4965a2944f6375737fc83ec6f3cbfa2d3aa0945c
Erwan8989 <77726753+Erwan8989@users.noreply.github.com>
2022-10-04 10:33:50 +0200

    ...

--- php/pages/profils.php
+++ php/pages/profils.php
@@ -9,7 +9,8 @@ $profil = "active";
     <meta charset="UTF-8" />
     <link rel="icon" type="image/svg+xml" href="../../images/favicon.png" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="stylesheet" href="../../style/style.css"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Workshop 2022</title>
 </head>
 <body>
@@ -19,6 +20,29 @@ $profil = "active";
         include "../composants/navbar.php"
         ?>
     </div>
+	<div class="row">
+		<h2>Veuillez choisir votre profil</h2>
+	</div
+	<div class="row">
+		<div class="col">
+			<div class="form-check form-switch">
+				<input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked>
+				<label class="form-check-label" for="flexSwitchCheckChecked">Céréalier</label>
+			</div>
+			<div class="form-check form-switch">
+				<input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+				<label class="form-check-label" for="flexSwitchCheckDefault">Viticulteur</label>
+			</div>
+			<div class="form-check form-switch">
+				<input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+				<label class="form-check-label" for="flexSwitchCheckDefault">Eleveur</label>
+			</div>
+			<div>
+				<input type="checkbox" id="test"/>
+				<label for="test">TEST</label>
+			</div>
+		</div>
+	</div>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
