1cc72af2dfccb0b661274ed71b4241013a6e1b64
Erwan8989 <77726753+Erwan8989@users.noreply.github.com>
2022-10-04 12:09:32 +0200

    Update accueil.php

--- php/pages/accueil.php
+++ php/pages/accueil.php
@@ -27,8 +27,8 @@ $profil = "";
 					<div class="card" style="">
 						<canvas id="myChart" width="200" height="50"></canvas>
 						<div class="card-body">
-							<div class="dropdown">
-								<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+							<div class="dropdown mb-2">
+								<button class="btn btn-success dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
 									Type de graphique
 								</button>
 								<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
@@ -37,7 +37,7 @@ $profil = "";
 									<li><a class="dropdown-item" href="#">Barre</a></li>
 								</ul>
 							</div>
-							<h5 class="card-title">Card title</h5>
+							<h5 class="card-title">Titre graphique</h5>
 							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 							<a href="#" class="btn btn-primary">Go somewhere</a>
 						</div>
@@ -54,31 +54,8 @@ $profil = "";
 					</div>
 				</div>
 			</div>
-			<div class="row mt-3">
-				<div class="col">
-					<div class="card" style="">
-						<img src="../../images" class="card-img-top" alt="...">
-						<div class="card-body">
-							<h5 class="card-title">Card title</h5>
-							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-							<a href="#" class="btn btn-primary">Go somewhere</a>
-						</div>
-					</div>
-				</div>
-				<div class="col">
-					<div class="card" style="">
-						<img src="../../images" class="card-img-top" alt="...">
-						<div class="card-body">
-							<h5 class="card-title">Card title</h5>
-							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-							<a href="#" class="btn btn-primary">Go somewhere</a>
-						</div>
-					</div>
-				</div>
-			</div>
-
-
 		</div>
+
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 		<script type="module" src="../../js/script.js"></script>
