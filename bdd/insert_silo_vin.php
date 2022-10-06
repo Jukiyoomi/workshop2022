4e79cd863c5b3d6a7a4655e8d2c341e3ea3c50e3
Erwan8989 <77726753+Erwan8989@users.noreply.github.com>
2022-10-06 16:31:59 +0200

    Add vin formulaire

--- bdd/insert_silo_vin.php
+++ bdd/insert_silo_vin.php
@@ -4,7 +4,7 @@ $db = new DataBase();
 if (isset($_POST['type']) && isset($_POST['nom']) && isset($_POST['capacite_max'])) {
 	if ($db->dbConnect()) {
 		if ($db->insert_silo("silo", $_POST['type'], $_POST['nom'], $_POST['capacite_max'])) {
-			echo "Insertion réussie";
+			header("Location: ../php/pages/accueil.php");
 		} else echo "Insertion fail";
 	} else echo "Error: Database connection";
 } else echo "All fields are required";
